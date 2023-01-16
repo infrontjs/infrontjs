9f272db387cd42436711ffba49235630a1e8cffa
benny00100 <info@bennyschuetz.com>
2023-01-16 17:07:24 +0100

    ADDED mocha as testing framework

---
+++ test/IF.test.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import * as IF from './../src/IF.js';
+
+it( 'Check for all IF exports exists', () =>
+{
+    assert.equal( "object" === typeof IF, true );
+    assert.equal( "function" === typeof IF.State, true );
+    assert.equal( "function" === typeof IF.App, true );
+    assert.equal( "function" === typeof IF.Helper, true );
+    assert.equal( "function" === typeof IF.L18n, true );
+    assert.equal( "function" === typeof IF.StateManager, true );
+    assert.equal( "function" === typeof IF.PathObject, true );
+    assert.equal( "function" === typeof IF.Http, true );
+    assert.equal( "function" === typeof IF.Router, true );
+    assert.equal( "function" === typeof IF.TemplateManager, true );
+    assert.equal( "function" === typeof IF.ViewManager, true );
+});

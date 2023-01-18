480b0e14498be803a9dd6b04fd5d1400661ede59
benny00100 <info@bennyschuetz.com>
2023-01-18 16:33:26 +0100

    INC Version to 0.9.1

--- src/core/App.js
+++ src/core/App.js
@@ -8,7 +8,7 @@ import { Helper } from "../util/Helper.js";
 import { PathObject } from "../util/PathObject.js";
 
 
-const VERSION = '0.9.0 ';
+const VERSION = '0.9.1';
 
 const DEFAULT_PROPS = {
     "uid" : null,
